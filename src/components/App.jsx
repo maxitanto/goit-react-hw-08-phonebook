179fdbdaf3fe3fbc471bee6f49e84c490d1ace17
Maksym Baldyniuk <frundzik1989@gmail.com>
2024-01-22 21:05:32 +0200

    everything is ready without styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import css from './App.module.css';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { ContactList } from './ContactList/ContactList';
+// import { Filter } from './Filter/Filter';
+// import css from './App.module.css';
 
 import { Layout } from './Layout';
 import { Route, Routes } from 'react-router-dom';
